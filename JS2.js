c1090c7a74225d75832d84787266386ce1460456
DorityTM <tatyana.mih.davidyuk@gmail.com>
2025-08-01 13:58:13 +0200

    Resolve conflicts

--- JS2.js
+++ JS2.js
@@ -1,9 +1,9 @@
 let n = 3;
-let nn = n + "5";
+let nn = n + String(n);
 nn = +nn;
 console.log(nn);
 console.log(typeof nn);
-let nnn = nn + "7";
+let nnn = nn + String(n);
 nnn = +nnn;
 console.log(nnn);
 console.log(typeof nnn);

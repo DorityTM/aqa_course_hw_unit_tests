cd8117aa93f9fae3f9baf6a3e98829dc206eef2b
DorityTM <tatyana.mih.davidyuk@gmail.com>
2025-08-13 22:40:28 +0200

    Added HW7 - task1 & task2

--- homework-solutions/js/hw-7/task-2.mjs
+++ homework-solutions/js/hw-7/task-2.mjs
@@ -4,8 +4,17 @@
 */
 
 function isPalindrom(word) {
-  // Ваш код
+ 
+if (typeof word !== 'string') {
+  return false;
 }
+  const cleanUpWord = word.toLowerCase();
+  const reversedWord = cleanUpWord.split('').reverse().join('');
+  return cleanUpWord === reversedWord;
+}
+
+console.log(isPalindrom('civic'));
+
 
 /*
  2. findLongestWords()
@@ -15,7 +24,26 @@ function isPalindrom(word) {
 */
 
 function findLongestWords(sentence) {
-  // Ваш код
+  
+if (typeof sentence !== 'string' || sentence.trim() === '') {
+  return []; 
 }
+
+  const word = sentence.trim().toLowerCase().split(' ');
+  let longestWord = [];
+  let maxWordLength = 0;
+
+ for (let i = 0; i < word.length; i++) {
+  if (word[i].length > maxWordLength) {
+    longestWord = [word[i]];
+    maxWordLength = word[i].length;
+  } else if (word[i].length === maxWordLength) {
+    longestWord.push(word[i]);
+  }
+}
+return longestWord;
+}
+
+console.log(findLongestWords('Never learn to any programming language if you do not want to go crazy'));
 
 export { isPalindrom, findLongestWords };

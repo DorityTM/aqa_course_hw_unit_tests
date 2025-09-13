76cc0bc8631f1f433c2fee716132e2bd1dbb032f
DorityTM <69254043+DorityTM@users.noreply.github.com>
2025-09-14 00:32:19 +0200

    Improve password validation error handling

--- homework-solutions/ts/hw-13/task-2.ts
+++ homework-solutions/ts/hw-13/task-2.ts
@@ -22,24 +22,51 @@
 //return true;
 //};
 
+//function validatePassword(password: string): boolean {
+//if (password.length < 8) {
+//    console.log("Password should be at least 8 characters long")
+//    return false;
+//} else if (!/[A-Z]/.test(password)) {
+//    console.log("Password should contain at least one uppercase letter");
+//    return false;
+//} else if (!/[a-z]/.test(password)) {
+//    console.log("Password should contain at least one lowercase letter");
+//    return false;
+//} else if (!/[0-9]/.test(password)) {
+//    console.log("Password should contain at least one number");
+//    return false;
+//} else if (!/\S/.test(password)) {
+//    console.log("Password should not consist of only whitespaces");
+//    return false;
+//}
+//return true;
+//};
+
 function validatePassword(password: string): boolean {
+const errors = []
+let isValid = true;
 if (password.length < 8) {
-    console.log("Password should be at least 8 characters long")
-    return false;
-} else if (!/[A-Z]/.test(password)) {
-    console.log("Password should contain at least one uppercase letter");
-    return false;
-} else if (!/[a-z]/.test(password)) {
-    console.log("Password should contain at least one lowercase letter");
-    return false;
-} else if (!/[0-9]/.test(password)) {
-    console.log("Password should contain at least one number");
-    return false;
-} else if (!/\S/.test(password)) {
-    console.log("Password should not consist of only whitespaces");
-    return false;
-}
-return true;
+    errors.push("Password should be at least 8 characters long")
+    isValid = false
+};
+if (!/[A-Z]/.test(password)) {
+    errors.push("Password should contain at least one uppercase letter");
+    isValid = false
+};
+if (!/[a-z]/.test(password)) {
+    errors.push("Password should contain at least one lowercase letter");
+    isValid = false
+};
+if (!/[0-9]/.test(password)) {
+    errors.push("Password should contain at least one number");
+    isValid = false
+};
+if (!password.trim()) {
+    errors.push("Password should not consist of only whitespaces");
+    isValid = false
+};
+errors.length ? console.log(errors.join("\n")) :  console.log("Password is valid");
+return isValid;
 };
 
 console.log(validatePassword("ptFA45Z7g"));
@@ -53,6 +80,5 @@ console.log(validatePassword("ABCDEFGH"));
 console.log(validatePassword("abcdEFGH"));
 console.log(validatePassword("        "));
 console.log(validatePassword("abcD1"));
-console.log(validatePassword("Abcdefg1"));
 console.log(validatePassword("A1      "));
 console.log(validatePassword("x5      "));

ab43d3c50cbb4242d66bbddb1a9ba5d1a2885b17
DorityTM <tatyana.mih.davidyuk@gmail.com>
2025-09-14 21:16:25 +0200

    Added Task-3 solutions for HW14.2

---
+++ homework-solutions/ts/hw-14/func getAvgSalary.ts
@@ -0,0 +1,21 @@
+// Напиши дженерик функцию getAvgSalary принимающая через запятую любой набор объектов,
+// у которых есть как минимум поле salary: number, и возвращается среднее арифметическое зарплат всех переданных объектов
+
+interface IEmployee {
+  id: number;
+  name: string;
+  position: string;
+  salary: number;
+};
+
+const employees: IEmployee[] = [
+  { id: 1, name: "Olive", position: "Senior Manual QA",  salary: 5000 },
+  { id: 2, name: "Alex", position: "Senior AQA",  salary: 6000 },
+  { id: 3, name: "Anatoliy", position: "Lead AQA",  salary: 7000 },
+];
+
+function getAvgSalary<T extends { salary: number }>(...args: T[]) {
+return args.reduce((r, o) => r + o.salary,0 )/args.length
+};
+
+console.log(getAvgSalary(...employees));
